7e77a984fbc09d0b2a3abbeb6c2fcc6b357ffa00
cph-mtnl <mtnl@cphbusiness.dk>
2022-02-07 16:20:21 +0100

    My take on a solution

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+    return (
+        <div className="container">
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "../logo.svg"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={reactLogo} width="40px" alt="React logo"/>
+            <h2>ReactFacts</h2>
+            <p>React Course - Project 1</p>
+        </nav>
+    );
+}
+
+export default Navbar;
